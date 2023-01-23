38a01c4d210b3a42c5c91cbaf3614374df85b95f
GioPa97 <giovannipagliara97@.com>
2023-01-23 17:00:58 +0100

    Api get okay

--- La Mia Pizzeria 1/Controllers/ApiPizzeController.cs
+++ La Mia Pizzeria 1/Controllers/ApiPizzeController.cs
@@ -1,13 +1,12 @@
 ﻿using La_Mia_Pizzeria_1.Database;
 using La_Mia_Pizzeria_1.Models;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Hosting;
 
 namespace La_Mia_Pizzeria_1.Controllers
 {
     [Route("api/[controller]/[action]")]
     [ApiController]
-   
+
     public class ApiPizzeController : ControllerBase
     {
         [HttpGet]
