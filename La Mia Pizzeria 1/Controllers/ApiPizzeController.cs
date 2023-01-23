d94fb2bbe62ac5b4d0b1040bdcba6b9b8e945c09
GioPa97 <giovannipagliara97@.com>
2023-01-23 15:52:24 +0100

    Creato ApiController

--- La Mia Pizzeria 1/Controllers/ApiPizzeController.cs
+++ La Mia Pizzeria 1/Controllers/ApiPizzeController.cs
@@ -1,23 +1,26 @@
 ﻿using La_Mia_Pizzeria_1.Database;
 using La_Mia_Pizzeria_1.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Hosting;
 
 namespace La_Mia_Pizzeria_1.Controllers
 {
     [Route("api/[controller]/[action]")]
     [ApiController]
+   
     public class ApiPizzeController : ControllerBase
     {
-
         [HttpGet]
         public IActionResult Get()
         {
             using (PizzeriaContext db = new PizzeriaContext())
             {
-                List<Pizza> pizze = db.Pizze.ToList<Pizza>();
-                return Ok(pizze);
+                List<Pizza> articoli = db.Pizze.ToList<Pizza>();
+
+                return Ok(articoli);
             }
         }
+
 
     }
 }
